6debe8096c58ecb6d1a16331a0163898b26bf04d
mk2634-create <mk2634@srmist.edu.in>
2026-03-20 08:58:34 +0530

    Added Use Case 1

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,13 @@
+
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("   Welcome to Book My Stay App   ");
+        System.out.println("   Hotel Booking System    ");
+        System.out.println("=================================");
+        System.out.println("Application Started Successfully!");
+    }
+}
+
